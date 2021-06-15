6852b302909c2d7382a1dab134df0fe7ad0c804a
mihatronych <dolgushin.mikhail131200@yandex.ru>
2021-06-15 18:02:46 +0500

    db_fixes

--- db.js
+++ db.js
@@ -6,13 +6,12 @@ module.exports = new Sequelize(
     process.env.DB_PASSWORD, // Пароль
     {
         dialect: 'postgres',
+        protocol: 'postgres',
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false // <<<<<<< YOU NEED THIS,
-            }
+            ssl: true,
+            native:true
         }
     }
 )
